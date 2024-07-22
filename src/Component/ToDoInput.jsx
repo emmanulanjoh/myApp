7afcf88118859f58a0aaeb92e6883d799a486c31
emma ando <emmanuelanjoh2016@gmail.com>
2024-07-22 01:17:08 -0700

    final code

--- src/Component/ToDoInput.jsx
+++ src/Component/ToDoInput.jsx
@@ -5,12 +5,3 @@ export default function ToDoInput(props) {
   const  {handlerAddTodos} = props
   const [todosValue, setTodosValue] = useState('')
 
-  return (
-    <header>
-        <input value={todosValue} onChange = {(e) => {
-          setTodosValue(e.target.value)
-        }}placeholder="To do "></input>
-        <button onClick={() => {handlerAddTodos(todosValue)}}>Add</button>
-    </header>
-  )
-}
